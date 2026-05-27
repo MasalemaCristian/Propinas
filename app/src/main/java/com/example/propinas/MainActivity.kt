472211307241d63c56b83597ac350e9400b0d9c2
MasalemaCristiano <kevinfuela175@gmail.com>
2026-05-26 20:14:36 -0500

    Cálculo de propina

--- app/src/main/java/com/example/propinas/MainActivity.kt
+++ app/src/main/java/com/example/propinas/MainActivity.kt
@@ -58,7 +58,42 @@ fun CalculadoraPropinas() {
 
         Spacer(modifier = Modifier.height(15.dp))
 
+        Row {
 
+            Button(
+                onClick = {
+
+                    val montoDouble = monto.toDoubleOrNull() ?: 0.0
+                    resultado = montoDouble * 0.10
+                }
+            ) {
+                Text("10%")
+            }
+
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Button(
+                onClick = {
+
+                    val montoDouble = monto.toDoubleOrNull() ?: 0.0
+                    resultado = montoDouble * 0.15
+                }
+            ) {
+                Text("15%")
+            }
+
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Button(
+                onClick = {
+
+                    val montoDouble = monto.toDoubleOrNull() ?: 0.0
+                    resultado = montoDouble * 0.20
+                }
+            ) {
+                Text("20%")
+            }
+        }
 
         Spacer(modifier = Modifier.height(25.dp))
 
@@ -67,6 +102,4 @@ fun CalculadoraPropinas() {
             fontSize = 22.sp
         )
     }
-
-
-    }
+}
