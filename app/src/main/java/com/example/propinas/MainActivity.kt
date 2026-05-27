4680efa679a270a009c419e7c0c710d126dea334
MasalemaCristiano <kevinfuela175@gmail.com>
2026-05-26 19:16:20 -0500

    Agregué TextFields y botón

--- app/src/main/java/com/example/propinas/MainActivity.kt
+++ app/src/main/java/com/example/propinas/MainActivity.kt
@@ -44,6 +44,25 @@ fun CalculadoraPropinas() {
 
         Spacer(modifier = Modifier.height(20.dp))
 
+        TextField(
+            value = monto,
+            onValueChange = { monto = it },
+            label = { Text("Monto de la cuenta") }
+        )
 
+        Spacer(modifier = Modifier.height(15.dp))
+
+        Text(
+            text = "Selecciona el porcentaje"
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        Spacer(modifier = Modifier.height(25.dp))
+
+        Text(
+            text = "Propina calculada: $resultado  ($)",
+            fontSize = 22.sp
+        )
     }
 }
